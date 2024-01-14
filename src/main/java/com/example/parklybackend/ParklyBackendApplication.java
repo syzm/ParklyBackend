1ddde07aefd85153b083d88f64b611e955d85562
yosyz <szymone25@gmail.com>
2024-01-14 13:29:10 +0100

    initial commit

---
+++ src/main/java/com/example/parklybackend/ParklyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.parklybackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParklyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParklyBackendApplication.class, args);
+	}
+
+}
